5bcb8867d0cac9f7576403b664320327378dfac3
Masbro <s2gotosingapore@gmail.com>
2020-01-16 12:17:37 +0700

    Menghapus Fungsi yang Ga Guna
    biar Ga Ribaet

--- app/Http/Controllers/MasakanController.php
+++ app/Http/Controllers/MasakanController.php
@@ -20,14 +20,8 @@ class MasakanController extends Controller
         $data['orders'] = order::with(['masakan'])->get();
         
 
-        // $orders = DB::table('orders')->get();
         return view('index',['masakans' => $masakans],$data);
 
-        // $orders = DB::table('orders')->get();
-        // return view('index',['orders' => $orders]);
-
-        // $orders = DB::table('orders')->get();
-        // return view('index',['orders' => $orders]);
         
     }
 
@@ -39,9 +33,6 @@ class MasakanController extends Controller
     public function simpan(Request $request)
     {
 
-    // $validatedData = $request->validate([
-    //     'id_masakan' => 'unique:orders',
-    // ]);
 
     $validasi = order::where('id_masakan', $request->id_masakan)->first();
     
@@ -66,65 +57,4 @@ class MasakanController extends Controller
 	 
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\masakan  $masakan
-     * @return \Illuminate\Http\Response
-     */
-    public function show(masakan $masakan)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\masakan  $masakan
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(masakan $masakan)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\masakan  $masakan
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request)
-    {
-        DB::table('orders')->where('id_masakan',$request->id_masakan)->update([
-		    'id_masakan' => $request->id_masakan,
-            'jumlah' => $request->jumlah,
-    ]);
-    
-	return redirect('/');
-       
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\masakan  $masakan
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(masakan $masakan)
-    {
-        //
-    }
 }
